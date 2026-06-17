f7578767f1e94fc0ab92c22dbb1fd29df6ed0342
Saurabh Suryavanshi <saurabhlm10@gmail.com>
2026-06-17 09:33:03 +0530

    feat: refactor command handling to use enum for built-in commands and add type command

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,13 @@
 import { createInterface } from "readline";
 
+enum BuiltIn {
+    ECHO = 'echo',
+    EXIT = 'exit',
+    TYPE = 'type'
+}
+
+const BuiltIns = new Set<string>(Object.values(BuiltIn))
+
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -15,14 +23,22 @@ rl.on("line", (cmd) => {
     const rest = trimmed.slice(command.length).trim();
     const args = trimmed.split(' ').slice(1).filter(arg => arg !== '')
 
-    if (command === 'exit') {
+    if (command === BuiltIn.EXIT) {
         console.log('Bye!')
         rl.close();
         return;
     }
-    else if (command === 'echo') {
+    else if (command === BuiltIn.ECHO) {
         console.log(rest)
-    } else {
+    }
+    else if (command === BuiltIn.TYPE) {
+        if (BuiltIns.has(rest)) {
+            console.log(`${rest} is a shell builtin`)
+        } else {
+            console.log(`${rest}: not found`)
+        }
+    }
+    else {
         console.log(`${command}: command not found`);
     }
     rl.prompt();
